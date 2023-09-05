4d8589f402d82d2b130d29ee95055e3031b6ceae
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-09-05 04:36:03 -0600

    feat: :lipstick: Actualizar estilos de perfil

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -4,31 +4,31 @@
         Editar perfil
         </x-slot>
 
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Profile') }}
-            </h2>
-        </x-slot>
+        <article class="px-4 md:px-10 -mt-24">
+            <div class="w-full mx-auto px-4">
+                <div
+                    class="relative flex flex-col break-words  mb-6 shadow-lg rounded-lg bg-stone-100 border-0 lg:max-w-screen-xl">
+                    <div class="rounded-t bg-white mb-0 px-6 py-6">
+                        <div class="text-center flex justify-between">
+                            <h6 class="text-blueGray-700 text-lg font-bold">
+                                Mi Cuenta
+                            </h6>
+                        </div>
+                    </div>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                    <div class="flex flex-col gap-6 xl:flex-row px-4 lg:px-10 py-10 w-full">
+
                         @include('admin.profile.partials.update-profile-information-form')
-                    </div>
-                </div>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                        <hr class="border-b-1 border-blueGray-300" />
+
                         @include('admin.profile.partials.update-password-form')
-                    </div>
-                </div>
 
-                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div class="max-w-xl">
+                        <hr class="border-b-1 border-blueGray-300" />
+
                         @include('admin.profile.partials.delete-user-form')
                     </div>
                 </div>
             </div>
-        </div>
+        </article>
 </x-admin-layout>
