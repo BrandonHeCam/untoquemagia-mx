5f3d82306b9bc5a5bdb4c5eeb5c992229b52ab5d
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-09-04 22:35:10 -0600

    Modificar middleware de autenticación

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,8 +20,10 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if (Auth::guard($guard)->check() && Auth::user()->hasRole('admin')) {
+                return to_route('products');
+            } elseif (Auth::guard($guard)->check() && Auth::user()->hasRole('customer')) {
+                return to_route('dashboard.customer');
             }
         }
 
