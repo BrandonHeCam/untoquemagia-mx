7acab6953974986a691b3a19d6d51f02232b1d55
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-09-06 04:41:45 -0600

    Generar slug de producto

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
-    function category(): BelongsTo
+    // function category(): BelongsTo
+    // {
+    //     return $this->belongsTo(Category::class);
+    // }
+
+    protected static function boot()
     {
-        return $this->belongsTo(Category::class);
+        parent::boot();
+
+        static::creating(function ($product) {
+            // Genera el slug automáticamente a partir del título
+            $product->slug = Str::slug($product->name);
+        });
     }
 }
