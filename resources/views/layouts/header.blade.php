4f05838972223b11e4e6103eb820769ea86b6f0f
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-09-06 13:38:53 -0600

    Agregar ruta de publos magicos

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -47,7 +47,7 @@
                         {{__('Inicio') }}
                     </x-nav-link>
 
-                    <x-nav-link hoverBgColor="greenRegilet" :href="route('info')" :active="request()->routeIs('info')">
+                    <x-nav-link hoverBgColor="greenRegilet" target="_blank" :href="'http://untoquedemagia.netlify.app'">
                         <svg xmlns="http://www.w3.org/2000/svg" class="block md:hidden" width="24" height="24"
                             viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                             stroke-linecap="round" stroke-linejoin="round">
